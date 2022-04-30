23433d1540cf81ac7587c62dae3728fc84edac33
saurabh592 <shubham.pathak9323@gmail.com>
2022-04-30 12:35:06 +0530

    inital start

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const path = require('path')
+const app = express();
+
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+app.set('views', __dirname + '/views');
+app.set('layout','layouts/layout');
+app.use(expressLayout);
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+app.get("/explore",(req,res)=>{
+    res.render('home');
+})
+app.get("/about",(req,res)=>{
+    res.render('about');
+})
+
+
+app.listen("3000",(req,res) =>{
+    console.log("server has started")
+})
